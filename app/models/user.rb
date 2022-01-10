beceab57180ebb56e7ff358083185a5e616ccde9
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-01-10 14:09:57 -0500

    🔥 Chore: User Model and specs tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+
+    # @user = User.find_by(email: email.strip, password_digest: password.strip)
+    @user = User.where("lower(email) = ?", email.strip.downcase).where("password_digest = ?", password).first
+
+    return @user
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,57 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     it 'should have password and password confirmation match' do
       @user = User.create(password: "123admin", password_digest: "123admin")
-      expect(@user.password).to equal(@user.password_digest)
+      expect(@user.password).to eq(@user.password_digest)
     end
 
+    it 'should have password and password confirmation not match' do
+      @user = User.create(password: "123admin", password_digest: "admin123")
+      expect(@user.password).not_to eq(@user.password_digest)
+    end
+
+    it 'should have a unique email' do
+      @user = User.new
+      @user.email = "adminadmin@gmail.com"
+
+      User.create(email: 'adminadmin@gmail.com', firstname: 'Adamin', lastname: '123', password: 'admin123', password_digest: 'admin123')
+      @existingUser = User.create(email: 'adminadmin@gmail.com', firstname: 'Adamin', lastname: '123', password: 'admin123', password_digest: 'admin123')
+
+      expect(@existingUser.errors.full_messages).not_to be_empty
+      puts @existingUser.errors.full_messages
+    end
+
+    it 'should have all required params' do
+      @user = User.create(email: 'adminaasdfdmin@gmail.com', firstname: 'Adamin', lastname: '123', password: 'admin123', password_digest: 'admin123')
+      expect(@user.errors.full_messages).to be_empty
+    end
+
+    it 'should have a password with minimum length' do
+      @user = User.create(email: 'adminadmin@gmail.com', firstname: 'Adamin', lastname: '123', password: 'ad', password_digest: 'ad')
+      expect(@user.errors.full_messages).not_to be_empty
+      puts @user.errors.full_messages
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it 'should have be able to authenticate with email and password' do
+      @user = User.create(email: 'adminadmin@gmail.com', firstname: 'Adamin', lastname: '123', password: 'admin123', password_digest: 'admin123')
+      @result = User.authenticate_with_credentials(@user.email, @user.password)
+
+      expect(@result).not_to be_nil
+    end
+
+    it 'should have be able to authenticate with email with spaces' do
+      @user = User.create(email: ' adminadmin@gmail.com ', firstname: 'Adamin', lastname: '123', password: 'admin123', password_digest: 'admin123')
+      @result = User.authenticate_with_credentials(@user.email, @user.password)
+
+      expect(@result).not_to be_nil
+    end
+
+    it 'should have be able to authenticate with email with wrong cases' do
+      @user = User.create(email: 'AdmInadmin@gmail.com', firstname: 'Adamin', lastname: '123', password: 'admin123', password_digest: 'admin123')
+      @result = User.authenticate_with_credentials(@user.email, @user.password)
+
+      expect(@result).not_to be_nil
+    end
   end
 end
